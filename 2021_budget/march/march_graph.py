0c1fcff1d736d8cf77dd630f389b0e03eea56d4d
N8 Rollins <nathanield.rollins@gmail.com>
2021-02-27 10:53:30 +0800

    Created the dictionary template to be used to make the graphs

---
+++ 2021_budget/march/march_graph.py
@@ -0,0 +1,38 @@
+
+expenses = [
+    
+        ]
+
+gsList = []
+eatList = []
+ratsList = []
+clothesList = []
+taobaoListList = []
+transportationList = []
+hotelsList = []
+skateboardingList = []
+otherList = []
+
+
+days = []
+for day in range(1,32):
+    days.append(day)
+
+groceriesSupplies = dict(zip(days,gsList))
+eatingOut = dict(zip(days,gsList))
+rats = dict(zip(days,gsList))
+clothes = dict(zip(days,gsList))
+taobao = dict(zip(days,gsList))
+transportation = dict(zip(days,gsList))
+hotels = dict(zip(days,gsList))
+skateboarding = dict(zip(days,gsList))
+other = dict(zip(days,gsList))
+
+
+
+
+print(groceriesSupplies)
+    
+#allDays = [days for day in range(1,32)]
+#print(allDays)
+
