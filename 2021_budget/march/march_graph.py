bc64b05197de092b0e68cf0f33f30201d2d118a2
N8 Rollins <nathanield.rollins@gmail.com>
2021-03-04 03:11:42 +0800

    Add Third day of March expenses

--- 2021_budget/march/march_graph.py
+++ 2021_budget/march/march_graph.py
@@ -1,17 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
 expenses = [
     
         ]
-
-gsList = ['0', '58']
-eatList = ['0', '25']
-ratsList = ['0', '0']
-clothesList = ['0', '0']
-taobaoListList = ['0', '0']
-transportationList = ['4', '18']
-hotelsList = ['0', '0']
-skateboardingList = ['0', '0']
-otherList = ['0', '0']
+gsList = [0, 58, 0]
+#gsList = ['0', '58', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+eatList = [0, 25, 21]
+#eatList = ['0', '25', '21', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+ratsList = [0, 0, 114]
+#ratsList = ['0', '0', '114', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+clothesList = [0, 0, 0]
+#clothesList = ['0', '0', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+taobaoList = [0, 0, 65]
+#taobaoListList = ['0', '0', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+transportationList = [4, 18, 5]
+#transportationList = ['4', '18', '5', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+hotelList = [0, 0, 0]
+#hotelsList = ['0', '0', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+skateboardingList = [0, 0, 0]
+#skateboardingList = ['0', '0', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+otherList = [0, 0, 0]
+#otherList = ['0', '0', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+paybackList = [0, 1100, 345]
+#paybackList = ['0', '1100', '345', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+utilitiesList = [100, 0, 0]
+#utilitiesList = ['100', '0', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
 
 
 days = []
@@ -30,8 +44,25 @@ other = dict(zip(days,gsList))
 
 
 
+x = days = np.linspace(1, 31, 3)
+#y = transportationList[:]
+fig, ax = plt.subplots()
+ax.grid()
 
-print(transportation)
+plt.figure(figsize=None,facecolor='magenta')
+
+plt.plot(x, transportationList, 'bd-')
+plt.plot(x, utilitiesList, "r-*")
+plt.plot(x, taobaoList, "g-*")
+plt.plot(x, ratsList, "y-*")
+plt.plot(x, eatList, "c-*")
+plt.plot(x, gsList, "m-*")
+plt.xlabel('Days', fontsize=16)
+plt.title('March', fontsize=26)
+plt.ylabel('Expenses', fontsize=16)
+plt.show()
+
+#print(transportation)
     
 #allDays = [days for day in range(1,32)]
 #print(allDays)
